a1309cb6740068a9c7e79a27380057e919a33c91
Kirill Krylov <curunir@yandex.ru>
2019-04-07 20:35:39 +0300

    Remove plugins from main application repo (they were moved to separate repo)

--- Plugins/Common/plugin_base_qobject.cpp
+++
@@ -1,9 +0,0 @@
-#if defined(PLUGIN_BASE_QOBJECT)
-#include "plugin_base.h"
-
-PluginBase::PluginBase(QObject *parent) :
-    QObject(parent)
-{
-    constructorInit();
-}
-#endif
--- Plugins/plugins.pro
+++
@@ -1,3 +0,0 @@
-TEMPLATE = subdirs
-
-SUBDIRS += $$files(Projects/*.pro, true)
