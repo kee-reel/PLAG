68ea3b9454eff873cd0115ae7e82c2957a8951ae
Kee Reel <c4rb0n_unit@protonmail.com>
2021-10-17 20:37:15 +0300

    Set OpenGL rendering API for windows

--- Application/Sources/application_pc.cpp
+++ Application/Sources/application_pc.cpp
@@ -8,6 +8,10 @@
 
 void Application::init()
 {
+#if defined(_WIN32) || defined(WIN32)
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGL);
+#endif
+
 #ifndef IS_CLI_APP
 	setWindowIcon(QIcon(QStringLiteral("://Resources/Logo256.png")));
 	this->resize(540, 960);
